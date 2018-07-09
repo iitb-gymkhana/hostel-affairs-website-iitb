50bd998a42a8a8014d56259c65a4a29cc10cc330
Rohit Prasad <rohitrp3@gmail.com>
2018-07-10 04:37:02 +0530

    Define structure of web page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
