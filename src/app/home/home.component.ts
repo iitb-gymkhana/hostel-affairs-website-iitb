c64e4ecf3dabdd37fa9bd761d06198d154412dac
Rohit Prasad <rohitrp3@gmail.com>
2018-07-10 20:44:59 +0530

    Display carousel of iitb images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,23 @@ import * as bulmaCarousel from 'bulma-carousel';
 })
 export class HomeComponent implements OnInit, AfterViewInit {
 
+  imagesList = [
+    { src: '1.jpg', title: '' },
+    { src: '2.jpg', title: '' },
+    { src: '3.jpg', title: '' },
+    { src: '4.jpg', title: '' },
+    { src: '5.jpg', title: '' },
+    { src: '6.jpg', title: '' },
+    { src: '7.jpg', title: '' },
+    { src: '8.jpg', title: '' },
+    { src: '9.bmp', title: '' },
+    { src: '10.jpg', title: '' },
+    { src: '11.jpg', title: '' },
+    { src: '12.jpg', title: '' },
+    { src: '13.jpg', title: '' },
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
