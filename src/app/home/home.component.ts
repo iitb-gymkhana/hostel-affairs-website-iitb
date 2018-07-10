b3f4ae8a2f018bee18f371eb797032b8106c9565
Rohit Prasad <rohitrp3@gmail.com>
2018-07-10 20:28:36 +0530

    Add bulma carousel css as dependency

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as bulmaCarousel from 'bulma-carousel';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    const carousels = bulmaCarousel.attach();
   }
 
 }
