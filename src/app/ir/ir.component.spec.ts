860d7e5565640a33865df77f606533a62cf0f7a2
Rohit Prasad <rohitrp3@gmail.com>
2018-07-12 01:56:28 +0530

    Define component for IR

---
+++ src/app/ir/ir.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IrComponent } from './ir.component';
+
+describe('IrComponent', () => {
+  let component: IrComponent;
+  let fixture: ComponentFixture<IrComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ IrComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IrComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
