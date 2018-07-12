41d2362de033f617a5d5022ff4a37d527e9ffeac
Rohit Prasad <rohitrp3@gmail.com>
2018-07-13 03:00:24 +0530

    Make ha council page responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,13 +14,33 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
+    const breakpoint = {};
+    breakpoint['refreshValue'] = function() {
+      this.value = window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content').replace(/\"/g, '');
+    };
+
+    const sidenavDisplay = function(device) {
+      if (device === 'desktop') {
+        $('.sidenav').show();
+        $('.mobile-sidenav').hide();
+        $('.navbar-burger').removeClass('is-active');
+      } else {
+        $('.sidenav').hide();
+      }
+    };
+
+    $(window).resize(function() {
+      breakpoint['refreshValue']();
+      sidenavDisplay(breakpoint['value']);
+    }).resize();
+
     $('.navbar-burger').click(function () {
       if ($(this).hasClass('is-active')) {
         $(this).removeClass('is-active');
-        $('.sidenav').hide();
+        $('.mobile-sidenav').hide();
       } else {
-        $('.sidenav').show();
         $(this).addClass('is-active');
+        $('.mobile-sidenav').show();
       }
     });
   }
