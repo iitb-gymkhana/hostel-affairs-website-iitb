33b2247619322db9797ef5cfb1f85fd13b4a295f
Rohit Prasad <rohitrp3@gmail.com>
2018-08-01 02:00:29 +0530

    Add council members

--- src/app/ha-council/ha-council.component.ts
+++ src/app/ha-council/ha-council.component.ts
@@ -31,7 +31,7 @@ export class HaCouncilComponent implements OnInit {
       name: 'Kirtesh Ajay Gadiya',
       emails: ['gadi12196@gmail.com'],
       numbers: ['9769957413', '9653452334'],
-      hostel: 'H2/102',
+      hostel: 'H2/223',
       fb: 'https://www.facebook.com/gadi1590',
       image: 'kirtesh.png'
     },
@@ -61,7 +61,7 @@ export class HaCouncilComponent implements OnInit {
       name: 'Akash Chikhalikar',
       emails: ['isecir@iitb.ac.in'],
       numbers: ['9867843415'],
-      hostel: 'H2/147',
+      hostel: 'H2/145',
       fb: 'https://www.facebook.com/AKC141096',
       image: 'akash.png'
     }
@@ -97,21 +97,81 @@ export class HaCouncilComponent implements OnInit {
   overallCoordinatorSARC = [
     {
       name: 'Samarth Agrawal',
-      emails: ['institute.alumni.sec@iitbombay.org'],
-      numbers: [''],
+      emails: ['samarth@sarc-iitb.org'],
+      numbers: ['7045800466'],
       hostel: 'H2',
       fb: 'https://www.facebook.com/samarth.friends20',
       image: 'samarth.png'
     }
   ];
 
+  overallCoordinatorHygieneCommittee = [
+    {
+      name: 'Vibha Talwar',
+      emails: ['kat.vibha@gmail.com'],
+      numbers: ['9878060676'],
+      hostel: 'H10/D311',
+      fb: 'https://www.facebook.com/cutipie.vibha',
+      image: 'vibha.png'
+    },
+    {
+      name: 'Lokesh Kesiraju',
+      emails: ['lokeshkesiraju@gmail.com'],
+      numbers: ['8828291963', '8919669927'],
+      hostel: 'H9/218',
+      fb: 'https://www.facebook.com/kesiraju.lokesh',
+      image: 'lokesh.png'
+    }
+  ];
+
+  overallCoordinatorSustainableCell = [
+    {
+      name: 'Aditya Devalla',
+      emails: ['devallaaditya456@gmail.com'],
+      numbers: ['7799323800', '8919254673'],
+      hostel: 'H9/228',
+      fb: 'https://www.facebook.com/aditya.devalla.90',
+      image: 'aditya.png'
+    },
+    {
+      name: 'Abhay Menon',
+      emails: ['s.abhaymenon@gmail.com'],
+      numbers: ['9769439840'],
+      hostel: 'H8/176',
+      fb: 'https://www.facebook.com/abhay.menon.71',
+      image: 'abhay.png'
+    }
+  ];
+
+  convenerSustainableCell = [
+    {
+      name: 'Harsha Pottluri',
+      emails: ['harshapotluri99@gmail.com'],
+      numbers: ['8897909999'],
+      hostel: 'H3/268',
+      fb: 'https://www.facebook.com/profile.php?id=100008396193291',
+      image: 'harsha.png'
+    },
+    {
+      name: 'Hrushikesh Bodas',
+      emails: ['hrushikeshcoc@gmai.com'],
+      numbers: ['9969560413'],
+      hostel: 'H2/47',
+      fb: 'https://www.facebook.com/hrushikesh.bodas',
+      image: 'hrushikesh.png'
+    }
+  ];
+
   posts = [
     { post: 'General Secretary of Hostel Affairs (GSHA)', members: this.GSHAs },
     { post: 'Institute Secretaries of Hostel Affairs (ISHAs)', members: this.ISHAs },
-    { post: 'Girsl Nominee', members: this.girlsNominee },
+    { post: 'Girls Nominee', members: this.girlsNominee },
     { post: 'Institute Secretary of International Relations', members: this.ISIR },
     { post: 'Institute System Administrators', members: this.ISAs },
-    { post: 'Overall Coordinator, Student Alumni Relations Cell', members: this.overallCoordinatorSARC }
+    { post: 'Overall Coordinator, Student Alumni Relations Cell', members: this.overallCoordinatorSARC },
+    { post: 'Overall Coordinator, Hygiene Committee', members: this.overallCoordinatorHygieneCommittee },
+    { post: 'Overall Coordinator, Sustainable Cell', members: this.overallCoordinatorSustainableCell },
+    { post: 'Convener, Sustainable Cell', members: this.convenerSustainableCell }
   ]
   constructor() { }
 
