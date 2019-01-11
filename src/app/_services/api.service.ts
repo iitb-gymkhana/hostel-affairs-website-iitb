91b18e26e46b0f0747cbc669791ea81fbd35a34c
kethan chauhan <kethan88@gmail.com>
2019-01-11 14:27:14 +0530

    environments added

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router'
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ApiService {
   constructor(private http: HttpClient, private router: Router) {
   }
 
-  private base_url = "http://localhost:3009/ha_website"
+  private base_url = environment.apiBaseUrl;
 
   //auth functions
   logout_user() {
@@ -24,77 +25,77 @@ export class ApiService {
 
   //admin user apis
   admin_login(body) {
-    return this.http.post<any>(`${this.base_url}/api/users/login`, body)
+    return this.http.post<any>(`${this.base_url}/users/login`, body)
   }
 
   // outlet apis
   get_food_outlets() {
-    return this.http.get(`${this.base_url}/api/outlets`)
+    return this.http.get(`${this.base_url}/outlets`)
   }
 
   get_outlet_by_id(id) {
-    return this.http.get<any>(`${this.base_url}/api/outlets/${id}`)
+    return this.http.get<any>(`${this.base_url}/outlets/${id}`)
   }
 
   add_food_outlet(body) {
-    return this.http.post(`${this.base_url}/api/outlets`, body)
+    return this.http.post(`${this.base_url}/outlets`, body)
   }
 
   edit_food_outlet(body) {
-    return this.http.put(`${this.base_url}/api/outlets`, body)
+    return this.http.put(`${this.base_url}/outlets`, body)
   }
 
   delete_food_outlet(id) {
-    return this.http.delete(`${this.base_url}/api/outlets/${id}`)
+    return this.http.delete(`${this.base_url}/outlets/${id}`)
   }
 
   // menu apis
   get_outlet_menus() {
-    return this.http.get(`${this.base_url}/api/outlet_menus`)
+    return this.http.get(`${this.base_url}/outlet_menus`)
   }
 
   get_food_outlet_menu(id) {
-    return this.http.get(`${this.base_url}/api/outlets/${id}/menu`)
+    return this.http.get(`${this.base_url}/outlets/${id}/menu`)
   }
 
   get_menu_by_id(id) {
-    return this.http.get(`${this.base_url}/api/outlet_menus/${id}`)
+    return this.http.get(`${this.base_url}/outlet_menus/${id}`)
   }
 
   add_menu(body) {
-    return this.http.post(`${this.base_url}/api/outlet_menus`, body)
+    return this.http.post(`${this.base_url}/outlet_menus`, body)
   }
 
   edit_menu(body) {
-    return this.http.put(`${this.base_url}/api/outlet_menus`, body)
+    return this.http.put(`${this.base_url}/outlet_menus`, body)
   }
 
   delete_menu(id) {
-    return this.http.delete(`${this.base_url}/api/outlet_menus/${id}`)
+    return this.http.delete(`${this.base_url}/outlet_menus/${id}`)
   }
 
   // rating apis
   get_all_ratings() {
-    return this.http.get(`${this.base_url}/api/ratings`)
+    return this.http.get(`${this.base_url}/ratings`)
   }
 
   get_ratings(outlet_id) {
-    return this.http.get(`${this.base_url}/api/outlets/${outlet_id}/ratings`)
+    return this.http.get(`${this.base_url}/outlets/${outlet_id}/ratings`)
   }
 
   get_rating_by_id(id) {
-    return this.http.get<any>(`${this.base_url}/api/ratings/${id}`)
+    return this.http.get<any>(`${this.base_url}/ratings/${id}`)
   }
 
   add_rating(body) {
-    return this.http.post(`${this.base_url}/api/ratings`, body)
+    return this.http.post(`${this.base_url}/ratings`, body)
   }
 
   edit_rating(body) {
-    return this.http.put(`${this.base_url}/api/ratings`, body)
+    return this.http.put(`${this.base_url}/ratings`, body)
   }
 
   delete_rating(id) {
-    return this.http.delete(`${this.base_url}/api/ratings/${id}`)
+    return this.http.delete(`${this.base_url}/ratings/${id}`)
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://gymkhana.iitb.ac.in/hostels/ha_website/api',
 };
