9102408bda812e997493421823bf3da282b4ae80
Rohit Prasad <rohitrp3@gmail.com>
2019-03-16 23:32:43 +0530

    Show status bar while loading eshuttle schedule

--- src/app/eshuttle/eshuttle.component.ts
+++ src/app/eshuttle/eshuttle.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { environment } from "../../environments/environment";
+import { NgProgress, NgProgressRef } from "@ngx-progressbar/core";
 
 @Component({
   selector: 'app-eshuttle',
   templateUrl: './eshuttle.component.html',
   styleUrls: ['./eshuttle.component.css']
 })
-export class EshuttleComponent implements OnInit {
+export class EshuttleComponent implements OnInit, AfterViewInit {
 
   scheduleUrl = environment.eshuttleSchedule;
-  
-  constructor() { }
+  progressBar: NgProgressRef;
+
+  constructor(
+    private progress: NgProgress
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.progressBar = this.progress.ref('progress-bar')
+    this.progressBar.start();
   }
 
 }
