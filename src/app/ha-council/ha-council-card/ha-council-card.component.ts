636a69a8e46a9782d75e9337aa04d577ed60c9b0
Rohit Prasad <rohitrp3@gmail.com>
2019-04-01 18:58:29 +0530

    Add year-wise data of hostel affairs council

--- src/app/ha-council/ha-council-card/ha-council-card.component.ts
+++ src/app/ha-council/ha-council-card/ha-council-card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HaCouncilCardComponent implements OnInit {
   @Input() member: any;
+  @Input() councilYear: string;
 
   constructor() { }
 
